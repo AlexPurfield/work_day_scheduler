998a236f7dcfa128f39dd478ee9882fbacd7a898
Alex Purfield <alexpurfield@gmail.com>
2023-09-15 18:22:18 -0600

    finished project: add loop fo time divs, and fixed class assignment for past, present and future classes; added to ReadMe

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
+//wrap js file in dom function
 
 $(function () { 
 var saveBtn = $(".saveBtn");
@@ -12,27 +9,42 @@ saveBtn.on('click' ,  function (event) {
 
   console.log ("EVENT: " + value);
   console.log (time);
-  localStorage.setItem(time, value); //save to localstorage with time as key and value as value 
+  localStorage.setItem(time, value); //save to localstorage with time as key and value as value so we can see the saved event with the correlating time
 
 })
-var currentHour = dayjs().format('H');
-console.log (currentHour);
+
 
 var displayDate = dayjs().format('MMM, D, YYYY');
-console.log (displayDate);
+console.log (displayDate); //getting current date to display at top of page 
+
+var displayP= $("#currentDay"); // value we are going to append to the element with a class of #currentDay
+
+displayP.append(displayDate); //appending the display date to the p element in the HTML
 
 
+// use this function to compare the current time to the time on the scheduler to see if it is past, present, or future 
+function hourUpdater()  {
+var timeNow = dayjs().format('h'); //timeNow is current in 12 hour clock 
+// console.log (timeNow);
+$(".time-block").each(function () { //.time-block is the class for each div so this should loop through each time div
+  var blockTime = parseInt($(this).attr("id").split("hour")[1]);
 
+//add class/color based on time
+  if (timeNow ===blockTime) {
+    $(this).addClass("present");
+  } else if (timeNow < blockTime) {
+    $(this).removeClass("present");
+    $(this).removeClass("past");
+    $(this).addClass("future");
+  } else if (timeNow > blockTime) {
+    $(this).removeClass("future");
+    $(this).removeClass("present");
+    $(this).addClass("past");
+  }
+})
+}
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+//call hourUpdater function
+hourUpdater();
+
+})
